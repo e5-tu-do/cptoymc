3dc345b20a2b486e8e7a054112659db20f6577db
Julian Wishahi <julian.wishahi@tu-dortmund.de>
2014-04-01 15:28:39 +0200

    headers.

--- main/DoToyGenMain.cpp
+++ main/DoToyGenMain.cpp
@@ -1,6 +1,23 @@
+// STL
+#include <iostream>
+#include <vector>
+
+// ROOT
+#include "TBranch.h"
+#include "TFile.h"
+#include "TTree.h"
+
+// RooFit – Basics
+#include "RooCategory.h"
+#include "RooFormulaVar.h"
+#include "RooRealVar.h"
+
+// RooFit – PDFs
+#include "RooGaussModel.h"
+#include "RooGaussian.h"
 
 int main() {
-
+  
 
   return 0;
 }
