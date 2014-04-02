deac3950e61181e2af85dfde77ce8385a93eda6e
Julian Wishahi <julian.wishahi@tu-dortmund.de>
2014-04-02 22:31:20 +0200

    Implemented CPV, time, and tagging!

--- main/DoToyGenMain.cpp
+++ main/DoToyGenMain.cpp
@@ -1,4 +1,5 @@
 // STL
+#include <cmath>
 #include <iostream>
 #include <vector>
 
@@ -33,6 +34,7 @@ struct ParsTimeAndCP {
   double Sf;
   double Cf;
   double Df;
+  double AP;
 };
 
 struct ParsMassResol  {
@@ -68,9 +70,11 @@ void generateTagAndEta(TRandom& rndm, const ParsTagging& pars_tagging, const int
 void generateTagAndEtaOS(TRandom& rndm, const ParsTagging& pars_tagging, const int tag_true, int& tag_OS, double& eta_OS);
 void generateTagAndEtaSS(TRandom& rndm, const ParsTagging& pars_tagging, const int tag_true, int& tag_SS, double& eta_SS);
 
+double BCPV_PDF(double t, double d, double tau, double dGamma, double dm, double Sf, double Cf, double Df);
+double BCPV_PDF_Envelope(double t, double gamma_min, double Sf, double Cf, double Df);
 
 int main() {
-  int num_events = 1000;
+  int num_events = 100000;
 
   //============================================================================
   // Physics Parameters
@@ -88,7 +92,8 @@ int main() {
   pars_time_CP.dm     = 0.510;
   pars_time_CP.Sf     = 0.7;
   pars_time_CP.Cf     = 0.0;
-  pars_time_CP.Df     = 0.0;
+  pars_time_CP.Df     = +sqrt(pars_time_CP.Sf*pars_time_CP.Sf+pars_time_CP.Cf*pars_time_CP.Cf);
+  pars_time_CP.AP     = 0.0;
 
   //============================================================================
   // Experimental Parameters
@@ -183,7 +188,38 @@ void generateTrueMass(TRandom& rndm, const ParsMass& pars_mass, double& obs_mass
 }
 
 void generateTrueTimeAndTag(TRandom& rndm, const ParsTimeAndCP& pars_time_CP, double& obs_time_true, int& obs_tag_true) {
+  // helper quantities
+  double prob_B = (1. - pars_time_CP.AP)/2.;
+  double gamma_min = 1./pars_time_CP.tau - std::abs(pars_time_CP.dGamma)/2.;
 
+  // local values of tag and time
+  int    val_d = 0;
+  double val_t = 0.;
+
+  // now hit and miss, stolen from BDecay!!!
+  double val_envelope = 0.;
+  double val_pdf      = 0.;
+  while(true) {   
+    // get initial flavour while taking production asymmetry into account
+    val_d = (rndm.Uniform() < prob_B) ? +1 : -1;
+  
+    // get time
+    val_t = -log(rndm.Uniform())/gamma_min;
+
+    // get pdf value and envelope value
+    val_pdf      = BCPV_PDF(val_t, val_d, pars_time_CP.tau, pars_time_CP.dGamma, pars_time_CP.dm, pars_time_CP.Sf, pars_time_CP.Cf, pars_time_CP.Df);
+    val_envelope = BCPV_PDF_Envelope(val_t, gamma_min, pars_time_CP.Sf, pars_time_CP.Cf, pars_time_CP.Df);
+
+    if (val_envelope < val_pdf) {
+      std::cout << "WARNING: Envelope smaller than PDF!" << std::endl;
+    }
+
+    if(val_envelope*rndm.Uniform() > val_pdf) continue;
+    else break;
+  }
+
+  obs_tag_true  = val_d;
+  obs_time_true = val_t;
 }
 
 
@@ -208,4 +244,12 @@ void generateTagAndEtaOS(TRandom& rndm, const ParsTagging& pars_tagging, const i
 
 void generateTagAndEtaSS(TRandom& rndm, const ParsTagging& pars_tagging, const int tag_true, int& tag_SS, double& eta_SS) {
 
+}
+
+double BCPV_PDF(double t, double d, double tau, double dGamma, double dm, double Sf, double Cf, double Df) {
+ return exp(-t/tau)*(cosh(dGamma*t/2.)+Df*sinh(dGamma*t/2.)+d*Cf*cos(dm*t)-d*Sf*sin(dm*t));
+}
+
+double BCPV_PDF_Envelope(double t, double gamma_min, double Sf, double Cf, double Df) {
+  return exp(-t*gamma_min)*(1.+std::abs(Df)+sqrt(Sf*Sf+Cf*Cf));
 }
