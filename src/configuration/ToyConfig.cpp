65b0564c75e054a9006457c26cb07eca5dea0c32
Julian Wishahi <julian.wishahi@tu-dortmund.de>
2014-05-09 16:42:48 +0200

    Configuration of yields works.

--- src/configuration/ToyConfig.cpp
+++ src/configuration/ToyConfig.cpp
@@ -1,5 +1,9 @@
 #include "configuration/ToyConfig.h"
 
+// from STDL
+#include <iostream>
+
+
 // from Boost - property_tree
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/info_parser.hpp>
@@ -23,20 +27,29 @@ void ToyConfig::load(const std::string& config_file) {
   using boost::property_tree::ptree;
   ptree pt;
 
-  // // parse input file
-  // boost::property_tree::info_parser::read_info(config_file, pt);
+  // parse input file
+  boost::property_tree::info_parser::read_info(config_file, pt);
 
-  // if (!pt.empty()){ 
-  //   for (auto pt_it: pt) {
-  //     std::cout << pt_it.first << std::endl;
-  //   }
-  // } else {
-  //   std::cout << "Tree is empty!" << std::endl;
-  // }
+  if (!pt.empty()){ 
+    for (auto pt_it: pt) {
+      // Prepare Components
+      
+    }
+  } else {
+    std::cout << "Tree is empty!" << std::endl;
+  }
 
-  comp_configs_.emplace("Sig_Bd",CompConfig("Sig_Bd",0,10000));
-  comp_configs_.emplace("Sig_Bs",CompConfig("Sig_Bs",0,100));
-  comp_configs_.emplace("Bkg",   CompConfig("Bkg",0,10000));
+  // get component_trees
+  auto pt_comp = pt.get_child("Components");
+  for (auto pt_comp_it: pt_comp) {
+    comp_configs_.emplace(
+      pt_comp_it.second.get<std::string>("name"),
+      CompConfig(pt_comp_it.second.get<std::string>("name"),
+                 pt_comp_it.second.get("comp_cat",-1000),
+                 pt_comp_it.second.get("yield",0))
+    );
+  }
+
   
   
 }
