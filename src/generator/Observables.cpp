eda79c1fd83fb6990351183b2f8e6a1474e6ebbc
Julian Wishahi <julian.wishahi@tu-dortmund.de>
2014-05-20 08:39:42 +0200

    Created Observable int and real classes.

--- src/generator/Observables.cpp
+++ src/generator/Observables.cpp
@@ -3,6 +3,25 @@
 namespace cptoymc {
 namespace generator {
 
+Observable::Observable(const std::string& dim_name, const std::string& var_name, const std::string& var_title) :
+  dim_name_(dim_name),
+  var_name_(var_name),
+  var_title_(var_title)
+  { }
+  
+ObservableReal::ObservableReal(const std::string& dim_name, const std::string& var_name, const std::string& var_title, double value, double min_value, double max_value) :
+  Observable(dim_name, var_name, var_title),
+  value_(value),
+  min_value_(min_value),
+  max_value_(max_value)
+{ }
+
+ObservableInt::ObservableInt(const std::string& dim_name, const std::string& var_name, const std::string& var_title, int value, const std::set<int>& valid_values) :
+  Observable(dim_name, var_name, var_title),
+  value_(value),
+  valid_values_(valid_values)
+  {}
+  
 
 Observables::Observables() :
   mass_true(-1000.),
--- src/generator/Observables.h
+++ src/generator/Observables.h
@@ -1,9 +1,70 @@
 #ifndef CPTOYMC_GENERATOR_OBSERVABLES_H
 #define CPTOYMC_GENERATOR_OBSERVABLES_H
 
+// from STL
+#include <iostream>
+#include <set>
+#include <string>
+
 namespace cptoymc {
 namespace generator {
 
+class Observable {
+public:
+  Observable(const std::string& dim_name, const std::string& var_name, const std::string& var_title);
+  virtual ~Observable() { };
+  
+  virtual bool HasValidValue() = 0;
+
+protected:
+  std::string dim_name_;
+  std::string var_name_;
+  std::string var_title_;
+};
+  
+class ObservableReal : public Observable {
+public:
+  ObservableReal(const std::string& dim_name, const std::string& var_name, const std::string& var_title, double value, double min_value, double max_value);
+  virtual ~ObservableReal() { };
+  
+  void set_range(double min_value, double max_value) {
+    min_value_ = min_value;
+    max_value_ = max_value;
+  }
+  
+  void set_value(double value) { value_ = value; }
+  double value() const { return value_; }
+  
+  virtual bool HasValidValue() {
+    return IsValidValue(value_);
+  }
+  
+  bool IsValidValue(double value) {
+    return (value < max_value_) && (value >= min_value_);
+  }
+  
+private:
+  double value_;
+  double min_value_;
+  double max_value_;
+};
+
+class ObservableInt : public Observable {
+public:
+  ObservableInt(const std::string& dim_name, const std::string& var_name, const std::string& var_title, int value, const std::set<int>& valid_values);
+  virtual ~ObservableInt() { };
+  
+  virtual bool HasValidValue() {return IsValidValue(value_);}
+  
+  bool IsValidValue(int value) {
+    return (valid_values_.find(value) != valid_values_.end());
+  }
+  
+private:
+  int value_;
+  std::set<int> valid_values_;
+};
+  
 class Observables {
 public:
   Observables();
