5d86842e936a6e1825079575cead7ecba7cbb909
Julian Wishahi <julian.wishahi@tu-dortmund.de>
2014-07-08 11:13:20 +0200

    Changed the handling of empty property_trees.

--- src/configuration/ToyConfig.cpp
+++ src/configuration/ToyConfig.cpp
@@ -30,21 +30,23 @@ void ToyConfig::load(const std::string& config_file) {
   // parse input file
   boost::property_tree::info_parser::read_info(config_file, pt);
 
-  if (!pt.empty()){
-    auto pt_comp = pt.get_child("Components");
-    for (auto pt_comp_it : pt_comp) {
-      comp_configs_.emplace(
-        pt_comp_it.first,
-        CompConfig(pt_comp_it.second.get<std::string>("name"),
-                   pt_comp_it.second.get("comp_cat",-1000),
-                   pt_comp_it.second.get("yield",0),
-                   pt_comp_it.second.get<std::string>("model","NoModel"),
-                   pt_comp_it.second.get_child("model"))
-      );
-    }
-  } else {
-    std::cout << "Tree is empty!" << std::endl;
+  if (pt.empty()) {
+    std::cout << "Tree is empty! Cannot configure the toy generator. Aborting." << std::endl;
+    return;
   }
+
+  auto pt_comp = pt.get_child("Components");
+  for (auto pt_comp_it : pt_comp) {
+    comp_configs_.emplace(
+      pt_comp_it.first,
+      CompConfig(pt_comp_it.second.get<std::string>("name"),
+                 pt_comp_it.second.get("comp_cat",-1000),
+                 pt_comp_it.second.get("yield",0),
+                 pt_comp_it.second.get<std::string>("model","NoModel"),
+                 pt_comp_it.second.get_child("model"))
+                 );
+  }
+
 
 }
 
