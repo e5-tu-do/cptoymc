ec03151b26ae11146976f64be063815e4edd11a5
Leonard Wollenberg <lwollenberg@lhcb.e5.physik.tu-dortmund.de>
2015-06-11 13:21:29 +0200

    Time fixed

--- main/P2VV_Generator.cpp
+++ main/P2VV_Generator.cpp
@@ -11,11 +11,11 @@
 #include <TMath.h>
 
 
-bool CosSquare(TRandom& Random, double& obs , double min , double max , double Omega = 1){ //accept and reject of cos^2
+bool CosSquare(TRandom& Random, double& obs , double min , double max , double sign = 1,double Omega = 1){ //accept and reject of cos^2
 	while( true ){
-		double RandomY =  Random.Rndm();
+		double RandomY =  sign * Random.Rndm();
 		double RandomX= max - (max - min) * Random.Rndm();
-		if(RandomY <= TMath::Cos(Omega * RandomX) * TMath::Cos(Omega * RandomX)){
+		if(RandomY <= sign * TMath::Cos(Omega * RandomX) * TMath::Cos(Omega * RandomX)){
 			obs = RandomX;
 			return true;
 		}
@@ -23,53 +23,51 @@ bool CosSquare(TRandom& Random, double& obs , double min , double max , double O
 	return false;
 }
 
-bool SinSquare(TRandom3& Random ,double& obs,  double min , double max , double Omega = 1){	//accept and reject of sin^2
+bool SinSquare(TRandom3& Random ,double& obs,  double min , double max , double sign = 1 , double Omega = 1){	//accept and reject of sin^2
 	while( true ){
-		double WertY =  Random.Rndm();
-		double WertX = max - (max - min) * Random.Rndm();
-		if(WertY <= TMath::Sin(Omega * WertX) * TMath::Sin(Omega * WertX)){
-			obs = WertX;
+		double RandomY = sign * Random.Rndm();
+		double RandomX = max - (max - min) * Random.Rndm();
+		if(RandomY <= sign * TMath::Sin(Omega * RandomX) * TMath::Sin(Omega * RandomX)){
+			obs = RandomX;
 			return true;
 		}
 	}
 	return false;
 }
 
-bool Sin(TRandom3& Random , double& obs, double min , double max , double Omega = 1){
-	if (TMath::Abs(Omega *( min - max ) ) < 2*TMath::Pi()){
+bool Sin_Trafo(TRandom3& Random , double& obs, double min , double max , double sign = 1, double Omega = 1){
+	/*if (TMath::Abs(Omega *( min - max ) ) < 2*TMath::Pi()){
 		double Alpha = TMath::Cos(Omega * min);
 		double Beta = TMath::Cos(Omega * max);
 		obs = 1/Omega * TMath::ACos((Beta - Alpha )*Random.Rndm()-Alpha);
 		return true;
-	}else {
+	}else {*/
 		while( true ){
-			double WertY = 1 - 2 *Random.Rndm();
-			double WertX = max - (max - min) * Random.Rndm();
-			if(WertY <= TMath::Sin(WertX * Omega)){
-				obs = WertX;
+			double RandomY = sign * (1 - 2 *Random.Rndm());
+			double RandomX = max - (max - min) * Random.Rndm();
+			if( RandomY <= sign * TMath::Sin(  RandomX * Omega)){
+				obs = RandomX;
 				return true;
 			}
 		}
-	}	
 	return false;
 }
 
-bool Cos(TRandom3& Random , double& obs, double min , double max , double Omega = 1){
-	if (TMath::Abs(Omega *( min - max ) ) < 2*TMath::Pi()){
+bool Cos_Trafo(TRandom3& Random , double& obs, double min , double max , double sign = 1 , double Omega = 1){
+	/*if (TMath::Abs(Omega *( min - max ) ) < 2*TMath::Pi()){
 		double Alpha = TMath::Sin(Omega * min);
 		double Beta = TMath::Sin(Omega * max);
 		obs = 1/Omega * TMath::ASin((Beta - Alpha )*Random.Rndm()-Alpha);
 		return true;
-	}else {
+	}else {*/
 		while( true ){
-			double WertY = 1 - 2 * Random.Rndm();
-			double WertX = max - (max - min) * Random.Rndm();
-			if(WertY <= TMath::Cos(WertX * Omega)){
-				obs = WertX;
+			double RandomY = sign * (1 - 2 * Random.Rndm());
+			double RandomX = max - (max - min) * Random.Rndm();
+			if(  RandomY <=   sign *TMath::Cos(RandomX * Omega)){
+				obs = RandomX;
 				return true;
 			}
 		}
-	}
 	return false;
 }
 
@@ -78,112 +76,115 @@ bool ConstTrafo(TRandom3& Random , double& obs , double min , double max){
 	return true;
 }
 
-bool ExpCos(TRandom3& Random , double& obs , double Lambda  , double Omega  , double min = 0  , double max = 18 ){
+bool ExpCos(TRandom3& Random , double& obs , double Lambda  , double Omega , int sign , double min = 0  , double max = 18 ){
 	double Alpha = TMath::Exp(-min * Lambda );
 	double Beta = TMath::Exp(-max * Lambda);
 	while( true ){
-		double WertX = -1/Lambda*TMath::Log(Alpha - (Alpha - Beta) *  Random.Rndm());
-		double WertY = 1 - 2 * Random.Rndm();
-		if(WertY <= TMath::Cos(Omega * WertX) ){
-			obs = WertX;
+		double RandomX = -1/Lambda*TMath::Log(Alpha - (Alpha - Beta) *  Random.Rndm());
+		double RandomY = 1 - 2 * Random.Rndm();
+		if(RandomY <= sign * TMath::Cos(Omega * RandomX) ){
+			obs = RandomX;
 			return true;
 		}
 	}
 	return false;
 }
 
-bool ExpSin(TRandom3& Random , double& obs , double Lambda  , double Omega  , double min = 0  , double max = 18 ){ 
+bool ExpSin(TRandom3& Random , double& obs , double Lambda  , double Omega  , int sign , double min = 0  , double max = 18 ){ 
 	double Alpha = TMath::Exp(-min * (Lambda));
 	double Beta = TMath::Exp(-max * (Lambda));	
 	while( true ){
-		double WertX = -1/Lambda*TMath::Log( Alpha - (Alpha - Beta) * Random.Rndm());
-		double WertY = 1 - 2 * Random.Rndm() ;
-		if(WertY <= TMath::Sin(Omega * WertX) ){
-			obs = WertX;
+		double RandomX = -1/Lambda*TMath::Log( Alpha - (Alpha - Beta) * Random.Rndm());
+		double RandomY = 1 - 2 * Random.Rndm() ;
+		if(RandomY <= TMath::Sin(sign * Omega * RandomX) ){
+			obs = RandomX;
 			return true;
 		}
 	}
 	return false;
 }
 
-bool ExpCosH(TRandom3& Random , double& obs , double Lambda  , double Omega  , double min = 0  , double max = 18 ){
+bool ExpCosH(TRandom3& Random , double& obs , double Lambda  , double Omega , int sign = 1 , double min = 0  , double max = 18 ){
 	double Alpha = TMath::Exp(-min * (Lambda + Omega));
 	double Beta = TMath::Exp(-max * (Lambda + Omega ));	
+	double RandomX;
+	double RandomY;
 	while( true ){
-		double WertX = -1/(Lambda + Omega)*TMath::Log(Alpha - (Alpha - Beta) *  Random.Rndm());
-		double WertY = Random.Rndm() * TMath::Exp(WertX);
-		if(WertY <= TMath::Cos(Omega * WertX) * TMath::Exp(WertX) ){
-			obs = WertX;
+		RandomX = -1/(Lambda + Omega)*TMath::Log(Alpha - (Alpha - Beta) *  Random.Rndm());
+		RandomY = sign * Random.Rndm() * TMath::Exp( ( Lambda + Omega ) * RandomX);
+		if(RandomY <= TMath::Cos(Omega * RandomX) * TMath::Exp(Lambda * RandomX) ){
+			obs = RandomX;
 			return true;
 		}
 	}
 	return false;
 }
 
-bool ExpSinH(TRandom3& Random , double& obs , double Lambda  , double Omega  , double min = 0  , double max = 18 ){
+bool ExpSinH(TRandom3& Random , double& obs , double Lambda  , double Omega  , int sign = 1 , double min = 0  , double max = 18 ){
 	double Alpha = TMath::Exp(-min * (Lambda + Omega));
-	double Beta = TMath::Exp(-max * (Lambda + Omega) );	
+	double Beta = TMath::Exp(-max * (Lambda + Omega));	
+	double RandomX;
+	double RandomY;
 	while( true ){
-		double WertX = -1/(Lambda + Omega)*TMath::Log( Alpha - (Alpha - Beta) * Random.Rndm());
-		double WertY = Random.Rndm() * TMath::Exp(WertX);
-		if(WertY <= TMath::SinH(Omega * WertX) * TMath::Exp(WertX) ){
-			obs = WertX;
+		RandomX = -1/(Lambda + TMath::Abs(Omega))*TMath::Log( Alpha - (Alpha - Beta) * Random.Rndm());
+		RandomY = sign *Random.Rndm() * TMath::Exp( ( Lambda + TMath::Abs(Omega) ) * RandomX);
+		if(RandomY <= TMath::SinH(sign * Omega * RandomX) * TMath::Exp(Lambda * RandomX) ){
+			obs = RandomX;
 			return true;
 		}
 	}
 	return false;
 }
 
-bool Time_Trafo(TRandom3& Random, double& obs,
+int Time_Trafo(TRandom3& Random, double& obs,
 				  double a ,double b , double c , double d , 
 				  double Gamma_s , double Delta_Gamma_s , double Delta_Mass_s , 
 				  double min = 0 ,double max = 18)
 {
-	/*double AlphaExp = TMath::Exp(-Gamma_s * min);
-	double BetaExp = TMath::Exp(-Gamma_s * max);
-	double AlphaCosh = TMath::CosH(Delta_Gamma_s/2 * min);
-	double BetaCosh = TMath::CosH(Delta_Gamma_s/2 * max);
-	double AlphaSinh = TMath::SinH(Delta_Gamma_s/2 * min);
-	double BetaSinh = TMath::SinH(Delta_Gamma_s/2 * max);
-	double AlphaCos = TMath::Cos(Delta_Mass_s * min);
-	double BetaCos = TMath::Cos(Delta_Mass_s * max);
-	double AlphaSin = TMath::Sin(Delta_Mass_s * min);
-	double BetaSin = TMath::Sin(Delta_Mass_s * max);
-
-	double IntCosh = (AlphaExp *( Gamma_s *AlphaCosh + Delta_Gamma_s * AlphaSinh)-BetaExp*( Gamma_s/2 *BetaCosh + Delta_Gamma_s * BetaSinh ))/(pow(Gamma_s,2)-pow( Delta_Gamma_s , 2 ));
-	double IntSinh = (AlphaExp *( Gamma_s *AlphaSinh + Delta_Gamma_s * AlphaCosh)-BetaExp*( Gamma_s/2 *BetaSinh + Delta_Gamma_s * BetaCosh ))/(pow(Gamma_s,2)-pow( Delta_Gamma_s , 2 ));
-	double IntCos = (AlphaExp *( Gamma_s *AlphaCos - Delta_Mass_s * AlphaSin) + BetaExp*( Gamma_s/2 *BetaSin - Delta_Mass_s * BetaCos ))/(pow(Gamma_s,2)+pow( Delta_Gamma_s , 2 ));
-	double IntSin = (AlphaExp *( Gamma_s *AlphaSin + Delta_Mass_s * AlphaCos) - BetaExp*( Gamma_s/2 *BetaSin + Delta_Mass_s * BetaCos ))/(pow(Gamma_s,2)+pow( Delta_Gamma_s , 2 ));
-	*/
-	double IntCosh = 1 ;
-	double IntSinh = 1 ;
-	double IntSin = 1 ;
-	double IntCos = 1 ;
-	double sum_Time = 	TMath::Abs(a * IntCosh) + TMath::Abs(b * IntSinh) + TMath::Abs(c * IntCosh) + TMath::Abs(d * IntSin);
+	double sum_Time = 	TMath::Abs(a) + TMath::Abs(b) + TMath::Abs(c) + TMath::Abs(d);
 	double TimeRndm = Random.Rndm();
-	if(sum_Time  * TimeRndm < TMath::Abs(a * IntCosh)){
-		ExpCosH( Random , obs , Gamma_s , Delta_Gamma_s/2);
-		return true;
-	}else if(sum_Time  * TimeRndm < TMath::Abs(a * IntCosh) + TMath::Abs(b * IntSinh)){
-		ExpSinH( Random , obs , Gamma_s , Delta_Gamma_s/2);
-		return true;
-	}else if(sum_Time  * TimeRndm < TMath::Abs(a * IntCosh) + TMath::Abs(b * IntSinh) + TMath::Abs(c * IntCosh)){
-		ExpCos( Random , obs , Gamma_s , Delta_Mass_s);
-		return true;
+	if(sum_Time  * TimeRndm < TMath::Abs(a)){
+		if (a < 0 )
+		{
+			ExpCosH( Random , obs , Gamma_s , Delta_Gamma_s/2 , -1 , min , max );			
+		} else{
+			ExpCosH( Random , obs , Gamma_s , Delta_Gamma_s/2 , +1 , min , max );
+		}
+		return 1;
+	}else if(sum_Time  * TimeRndm < TMath::Abs(a) + TMath::Abs(b)){
+		if( b < 0){
+			ExpSinH( Random , obs , Gamma_s , Delta_Gamma_s/2 , -1 , min , max );	
+		} else{
+			ExpSinH( Random , obs , Gamma_s , Delta_Gamma_s/2 , +1 , min , max );
+		}
+		return 2;
+	}else if(sum_Time  * TimeRndm < TMath::Abs(a) + TMath::Abs(b) + TMath::Abs(c)){
+		if (c < 0)
+		{
+			ExpCos( Random , obs , Gamma_s , Delta_Mass_s , -1 , min , max );
+		}else{
+			ExpCos( Random , obs , Gamma_s , Delta_Mass_s , +1 , min , max );	
+		}
+		return 3;
 	}else{	
-		ExpSin( Random , obs , Gamma_s , Delta_Mass_s); 
-		return true;
+		if(c < 0){
+			ExpSin( Random , obs , Gamma_s , Delta_Mass_s , -1 , min , max );
+		}else{	
+			ExpSin( Random , obs , Gamma_s , Delta_Mass_s , +1 , min , max ); 
+		}
+		return 4;
 	}
-	return false;
+	return 5;
 }
 
 int main()
 {
 	auto start_time = std::chrono::high_resolution_clock::now();
 
-	unsigned long seed =1234567; 
-	double Daten = 1e6;
-	TRandom3 Random(seed);
+		unsigned long seed =1234567; 
+		double Daten = 1e6;
+		double DatenTime = 1;
+		TRandom3 Random(seed);
 
 	//Const	
 		double Delta_Mass_s = 17.757;
@@ -202,6 +203,8 @@ int main()
 		double Phase_0 = 0 ;
 		double Phase_Parallel = 2.71;
 		double Phase_Vertical = 2.39;
+		//double Phase_Parallel = 2.40;
+		//double Phase_Vertical = 2.39;
 
 		double CP_Lambda = 1;
 		double CP_Phi_sss = 0; // = 0.17
@@ -218,9 +221,9 @@ int main()
 		double Value_1 = 4 * Amplitude_0 * Amplitude_0;
 		double Value_2 = 2 * Amplitude_Parallel * Amplitude_Parallel;
 		double Value_3 = 2 * Amplitude_Vertical * Amplitude_Vertical;
-		double Value_4 = 2 * Amplitude_Parallel * Amplitude_Vertical;
-		double Value_5 = TMath::Sqrt(2)*Amplitude_Parallel * Amplitude_0;
-		double Value_6 = TMath::Sqrt(2)*Amplitude_0 * Amplitude_Vertical;
+		double Value_4 = Amplitude_Parallel * Amplitude_Vertical;
+		double Value_5 = Amplitude_Parallel * Amplitude_0;
+		double Value_6 = Amplitude_0 * Amplitude_Vertical;
 
 		double Sum = Value_1 + Value_2 + Value_3 + Value_4 + Value_5 + Value_6;
 
@@ -228,10 +231,12 @@ int main()
 
    		TFile *root_file= new TFile("P2VV.root", "RECREATE");
    		TTree *tree = new TTree("P2VV" , "");
-   		double Test;
-   		tree->Branch("Test" , &Test , "Test/D" );
+   		//double Test;
+   		//tree->Branch("Test" , &Test , "Test/D" );
    		double Time;
    		tree->Branch("Time" , &Time , "Time/D" );
+   		double TimeCut;
+   		tree->Branch("TimeCut" , &TimeCut , "TimeCut/D" );
 		double theta1;
    		tree->Branch("theta1",&theta1,"theta1/D");
    		double theta2;
@@ -240,66 +245,82 @@ int main()
    		tree->Branch("Phi" , &Phi , "Phi/D");
    		int cut;
    		tree->Branch("cut" , &cut , "cut/I");
+   		//std::cout << Value_1/4 +Value_2/2 +Value_3/2 << std::endl;
 
 	for (int i = 0 ; i < Daten ; ++i)
 	{
 		double RndmValue = Random.Rndm();
 
 		if(( Sum * RndmValue < Value_1)){
-			CosSquare(Random , theta1 , 0 , PI);
-			CosSquare(Random , theta2 , 0 , PI);
-			ConstTrafo(Random , Phi , -PI , PI);
-			Time_Trafo(Random , Time ,
-							  1 , CPV_D , CPV_C , -CPV_S ,
-							  Gamma_s, Delta_Gamma_s , Delta_Mass_s,  0 , 18);
-			cut = 1;
+			for(int j = 0 ; j < DatenTime ; ++j){
+				CosSquare(Random , theta1 ,  0  , PI , 1 , 1 );
+				CosSquare(Random , theta2 ,  0  , PI , 1 , 1 );
+				ConstTrafo(Random , Phi   , -PI , PI , 1 , 1 );
+				TimeCut = Time_Trafo(Random , Time ,
+								1 , CPV_D , CPV_C , -CPV_S ,
+								Gamma_s, Delta_Gamma_s , Delta_Mass_s,  0 , 18);
+				cut = 1;
+				tree->Fill();
+			}
 		}else if( Sum * RndmValue < Value_1 + Value_2){
-			SinSquare(Random , theta1 , 0 , PI);
-			SinSquare(Random , theta2 , 0 , PI);
-			SinSquare(Random , Phi , -PI , PI);
-			Time_Trafo(Random , Time ,
-							  1 , CPV_D , CPV_C , -CPV_S ,
-							  Gamma_s, Delta_Gamma_s , Delta_Mass_s,  0 , 18);
-			cut = 2;
+			for(int j = 0 ; j < DatenTime ; ++j){
+				SinSquare(Random , theta1 ,  0  , PI , 2 , 1);
+				SinSquare(Random , theta2 ,  0  , PI , 2 , 1);
+				CosSquare(Random , Phi    , -PI , PI , 4 , 1);
+				TimeCut = Time_Trafo(Random , Time ,
+									 1 , CPV_D , CPV_C , -CPV_S ,
+									 Gamma_s , Delta_Gamma_s , Delta_Mass_s,  0 , 18);
+				cut = 2;
+				tree->Fill();
+			}
 		}else if(Sum * RndmValue < Value_1 + Value_2 + Value_3){
-			SinSquare(Random , theta1 , 0 , PI);
-			SinSquare(Random , theta2 , 0 , PI);
-			CosSquare(Random ,Phi, -PI , PI);
-			Time_Trafo(Random , Time ,
-							  1 , -CPV_D , CPV_C , CPV_S ,
-							  Gamma_s, Delta_Gamma_s , Delta_Mass_s,  0 , 18);
-			cut = 3;
+			for(int j = 0 ; j < DatenTime ; ++j){
+				SinSquare(Random , theta1 ,  0  , PI , 1 , 1);
+				SinSquare(Random , theta2 ,  0  , PI , 1 , 1);
+				SinSquare(Random , Phi    , -PI , PI , 1 , 1);
+				TimeCut = Time_Trafo(Random , Time ,
+									 1 , -CPV_D , CPV_C , CPV_S ,
+									 Gamma_s    , Delta_Gamma_s , Delta_Mass_s,  0 , 18);
+				cut = 3;
+				tree->Fill();
+			}
 		}else if(Sum * RndmValue < Value_1 + Value_2 + Value_3 + Value_4){
-			SinSquare(Random , theta1 , 0 , PI, 1);
-			SinSquare(Random , theta2 , 0 , PI , 1);
-			Sin(Random , Phi , -PI , PI , -2);
-			Time_Trafo(Random , Time ,
-							  CPV_C * TMath::Sin(Phase_1) , CPV_S * TMath::Sin(Phase_1), TMath::Sin(Phase_1) , CPV_D*TMath::Sin(Phase_1) ,
-							  Gamma_s, Delta_Gamma_s , Delta_Mass_s,  0 , 18);
-
-			cut = 4;
+			for(int j = 0 ; j < DatenTime ; ++j){
+				SinSquare(Random , theta1 ,  0  , PI ,  1 , 1);
+				SinSquare(Random , theta2 ,  0  , PI ,  1 , 1);
+				Sin_Trafo(Random , Phi    , -PI , PI , -1 , 2);
+				TimeCut = Time_Trafo(Random , Time ,
+								CPV_C * TMath::Sin(Phase_1) , CPV_S * TMath::Sin(Phase_1), TMath::Sin(Phase_1) , CPV_D*TMath::Sin(Phase_1) ,
+								Gamma_s, Delta_Gamma_s , Delta_Mass_s,  0 , 18);
+				cut = 4;
+				tree->Fill();
+			}
 		}else if(Sum * RndmValue < Value_1 + Value_2 + Value_3 + Value_4 + Value_5){
-			Sin(Random , theta1 , 0 , PI , 2);
-			Sin(Random , theta2 , 0 , PI , 2);
-			Cos(Random , Phi , -PI , PI , 1);
-			Time_Trafo(Random , Time ,
-					   TMath::Sin(Phase_Parallel) ,CPV_D * TMath::Sin(Phase_Parallel), TMath::Sin(Phase_2) , -CPV_S *TMath::Sin(Phase_Parallel) ,
-					   Gamma_s, Delta_Gamma_s , Delta_Mass_s ,  0 , 18);
-
-			cut = 5;
+			for(int j = 0 ; j < DatenTime ; ++j){
+				Sin_Trafo(Random , theta1 ,  0  , PI , 1 , 2);
+				Sin_Trafo(Random , theta2 ,  0  , PI , 1 , 2);
+				Cos_Trafo(Random , Phi    , -PI , PI , 1 , 1);
+				TimeCut = Time_Trafo(Random , Time ,
+						   TMath::Sin(Phase_Parallel) ,CPV_D * TMath::Sin(Phase_Parallel), TMath::Sin(Phase_2) , -CPV_S *TMath::Sin(Phase_Parallel) ,
+						   Gamma_s, Delta_Gamma_s , Delta_Mass_s ,  0 , 18);
+				cut = 5;
+				tree->Fill();
+			}
 		}else if(Sum * RndmValue < Value_1 + Value_2 + Value_3 + Value_4 + Value_5 + Value_6){
-			Sin(Random , theta1 , 0 , PI , -2);
-			Sin(Random , theta2 , 0 , PI , -2);
-			Sin(Random , Phi , -PI , PI , -1);
-			Time_Trafo(Random , Time ,
-					   CPV_C * TMath::Sin(Phase_2) , CPV_S * TMath::Cos(Phase_2) , TMath::Sin(Phase_2) , CPV_D * TMath::Cos(Phase_2) ,
-					   Gamma_s, Delta_Gamma_s , Delta_Mass_s,  0 , 18);
-
-			cut = 6;
+			for(int j = 0 ; j < DatenTime ; ++j){			
+				Sin_Trafo(Random , theta1 , 0   , PI , -1 , 2);
+				Sin_Trafo(Random , theta2 , 0   , PI , -1 , 2);
+				Sin_Trafo(Random , Phi    , -PI , PI , -1 , 1);
+				TimeCut = Time_Trafo(Random , Time ,
+						   CPV_C * TMath::Sin(Phase_2) , CPV_S * TMath::Cos(Phase_2) , TMath::Sin(Phase_2) , CPV_D * TMath::Cos(Phase_2) ,
+						   Gamma_s, Delta_Gamma_s , Delta_Mass_s,  0 , 18);
+				cut = 6;
+				tree->Fill();
+			}
 		}
-		ExpCos( Random , Test , Gamma_s , Delta_Mass_s); 
+		//ExpCos( Random , Test , Gamma_s , Delta_Mass_s); 
 
-		tree->Fill();
+		//tree->Fill();
 
 	}
    	root_file->Write("P2VV");
