929d923f0c84bd76e9f18c12a8760a926dfa5f2c
Alex Birnkraut <a.birnkraut@cern.ch>
2016-05-30 16:57:12 +0200

    tidied up generator.cpp

--- src/generator/generator.cpp
+++ src/generator/generator.cpp
@@ -92,30 +92,19 @@ bool GenerateCPV_P2PV(TRandom& rndm, double par_prod_asym, double par_det_asym,
       else break;
     }
     else{
-      val_pdf = BCPV_PDF(val_t, val_d, par_tau, par_dGamma, par_dm, par_Sf, par_Cf, par_Df);
-      val_bar_pdf = BCPV_bar_PDF(val_t, val_d, par_tau, par_dGamma, par_dm, par_Sfbar, par_Cfbar, par_Dfbar);
-
-      double prob_finalstate = val_pdf/(val_pdf + val_bar_pdf);
       double prob_detect = (1 - par_det_asym)/2;
-
-      // std::cout << "val_pdf = " << val_pdf << std::endl;
-      // std::cout << "val_bar_pdf = " << val_bar_pdf << std::endl;
-      // std::cout << "prob_detect = " << prob_detect << std::endl;
-      // std::cout << "C = " << par_Cf << std::endl;
-      // std::cout << "Cbar = " << par_Cfbar << std::endl;
-      // std::cout << "combined = " << val_pdf/(val_pdf + val_bar_pdf) << std::endl;
-      // std::cout << "combined bar = " << val_bar_pdf/(val_pdf + val_bar_pdf) << std::endl;
-      // std::cout << "prob_finalstate = " << prob_finalstate << "\n" << std::endl;
 
       val_final = (rndm.Uniform() < prob_detect) ? +1 : -1;
 
       if(val_final  == 1) {
+        val_pdf = BCPV_PDF(val_t, val_d, par_tau, par_dGamma, par_dm, par_Sf, par_Cf, par_Df);
         val_envelope = BCPV_PDF_Envelope(val_t, gamma_min, par_Sf, par_Cf, par_Df);
         if (val_envelope < val_pdf) std::cout << "WARNING: Envelope smaller than PDF!" << std::endl;
         if(val_envelope*rndm.Uniform() > val_pdf) continue;
         else break;
       }
       else {
+        val_bar_pdf = BCPV_bar_PDF(val_t, val_d, par_tau, par_dGamma, par_dm, par_Sfbar, par_Cfbar, par_Dfbar);
         val_envelope = BCPV_PDF_Envelope(val_t, gamma_min, par_Sfbar, par_Cfbar, par_Dfbar);
         if (val_envelope < val_bar_pdf) std::cout << "WARNING: Envelope smaller than PDF!" << std::endl;
         if(val_envelope*rndm.Uniform() > val_bar_pdf) continue;
